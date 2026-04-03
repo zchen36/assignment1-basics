b5d79b41829cc71d8c67064e588425fe3d82538f
Z Chen <zchen@Zs-MacBook-Pro.attlocal.net>
2026-04-03 13:47:19 -0700

    embedding changes

--- src/llm/embedding.py
+++ src/llm/embedding.py
@@ -1,9 +1,9 @@
-from jaxtyping import Float
+from jaxtyping import Float, Int
 import torch
 from torch import Tensor, nn
 
 
-class Embdding(nn.Module):
+class Embedding(nn.Module):
     def __init__(self, vocab_size: int, embedding_dim: int, device=None, dtype=None):
         super().__init__()
         self.vocab_size = vocab_size
@@ -11,7 +11,14 @@ class Embdding(nn.Module):
         self.weight = nn.Parameter(torch.empty((vocab_size, embedding_dim), device=device, dtype=dtype))
         nn.init.trunc_normal_(self.weight, mean=0.0, std=1, a=-3, b=3)
 
-    def forward(
-        self, x: Float[Tensor, "batch sequence_length"]
-    ) -> Float[Tensor, "batch sequence_length embedding_dim"]:
+    def forward(self, x: Int[Tensor, "batch sequence_length"]) -> Float[Tensor, "batch sequence_length embedding_dim"]:
         return self.weight[x]
+
+
+if __name__ == "__main__":
+    embedding = Embedding(10, 5)
+    print(embedding.weight)
+
+    x = torch.tensor([[1, 2, 3], [4, 5, 6]])
+    print(embedding(x))
+    print("hello")
